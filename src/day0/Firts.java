3eaf1d70b0ffb0e37a9451cecbc1edb947f58f15
khayalabva <khayalabva@gmail.com>
2022-12-11 17:27:04 +0100

    Initial commit

---
+++ src/day0/Firts.java
@@ -0,0 +1,7 @@
+package day0;
+
+public class Firts {
+    public static void main(String[] args) {
+        System.out.printf("blah blah");
+    }
+}
---
+++ src/day0/HelloWord.java
@@ -0,0 +1,7 @@
+package day0;
+
+public class HelloWord {
+    public static void main(String[] args) {
+
+    }
+}
